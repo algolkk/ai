08a989e7dfd810e34496bb78f62d974d9a8bcc90
algolkk <154591466+algolkk@users.noreply.github.com>
2025-01-29 19:08:26 +0900

    fix: emoji-reaction

--- src/modules/emoji-react/index.ts
+++ src/modules/emoji-react/index.ts
@@ -37,21 +37,21 @@ export default class extends Module {
 		};
 
 		const customEmojis = note.text.match(/:([^\n:]+?):/g);
-		// if (customEmojis) {
-		// 	// カスタム絵文字が複数種類ある場合はキャンセル
-		// 	if (!customEmojis.every((val, i, arr) => val === arr[0])) return;
+		if (customEmojis) {
+			// カスタム絵文字が複数種類ある場合はキャンセル
+			if (!customEmojis.every((val, i, arr) => val === arr[0])) return;
 
-		// 	this.log(`Custom emoji detected - ${customEmojis[0]}`);
+			this.log(`Custom emoji detected - ${customEmojis[0]}`);
 
-		// 	return react(customEmojis[0]);
-		// }
+			return react(customEmojis[0]);
+		}
 
-		// const emojis = parse(note.text).map(x => x.text);
-		// if (emojis.length > 0) {
-		// 	// 絵文字が複数種類ある場合はキャンセル
-		// 	if (!emojis.every((val, i, arr) => val === arr[0])) return;
+		const emojis = parse(note.text).map(x => x.text);
+		if (emojis.length > 0) {
+			// 絵文字が複数種類ある場合はキャンセル
+			if (!emojis.every((val, i, arr) => val === arr[0])) return;
 
-		// 	this.log(`Emoji detected - ${emojis[0]}`);
+			this.log(`Emoji detected - ${emojis[0]}`);
 
 		// 	let reaction = emojis[0];
 
@@ -62,13 +62,14 @@ export default class extends Module {
 		// 	}
 
 		// 	return react(reaction);
-		// }
+		}
 
 		if (includes(note.text, ['カレー'])) return react('🍛');
 		if (includes(note.text, ['唐揚げ'])) return react('blobchicken');
 		if (includes(note.text, ['プリン'])) return react('🍮');
 		if (includes(note.text, ['寿司', 'sushi']) || note.text === 'すし') return react('🍣');
-		if (includes(note.text, ['ご飯', 'ごはん'])) return react('🍚');
+		// if (includes(note.text, ['ご飯', 'ごはん'])) return react('🍚');
+		if (includes(note.text, ['おなかすいた', 'onaka_suita_si'])) return react('🍚');
 
 		if (includes(note.text, ['Rikki'])) return react('🐱');
 	}
